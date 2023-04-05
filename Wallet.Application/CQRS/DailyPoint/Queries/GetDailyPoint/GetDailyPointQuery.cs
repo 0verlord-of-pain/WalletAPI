f63c923bde24448a754441eb82adaca9a2ab4d68
0verlord-of-pain <49004736+0verlord-of-pain@users.noreply.github.com>
2023-04-05 21:10:31 +0300

    Add InfoController
    Add Get Payment Info
    Add Get Daily Point

---
+++ Wallet.Application/CQRS/DailyPoint/Queries/GetDailyPoint/GetDailyPointQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Wallet.Application.CQRS.DailyPoint.Queries.GetDailyPoint;
+public class GetDailyPointQuery : IRequest<string>
+{
+    public GetDailyPointQuery(Guid userId)
+    {
+        UserId = userId;
+    }
+    
+    public Guid UserId { get; init; }
+}
